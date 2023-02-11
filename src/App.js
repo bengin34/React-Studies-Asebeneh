4e4f46c0ddb57601ba279ba74d653656322b7354
bengin34 <becaglar3434@gmail.com>
2023-02-11 20:58:07 +0300

    folder structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+//named export in arrow function
+// export const App = () => <h1>Welcome to 30 days Of React</h1>
+
+
+
+//named export in regular function , function decleration
+export function App () {
+    return <h1>Welcome to 30 days Of React</h1>
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM  from 'react-dom'
+
+const App = () => <h1>Welcome to 30 days of React</h1>
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />,rootElement)
