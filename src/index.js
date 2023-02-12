9eba50986884d94bc301457a8ed91f9b37ef9076
bengin34 <becaglar3434@gmail.com>
2023-02-12 23:25:52 +0300

    day18

---
+++ src/index.js
@@ -0,0 +1,220 @@
+// import { render } from '@testing-library/react'
+// import React, { Component } from 'react'
+// import ReactDOM from 'react-dom'
+
+// const Country = ({
+//     country: {name, capital, flag, languages, population, currency}, 
+// }) => {
+//     const formatedCapital = capital.length > 0 ? (
+//         <>
+//             <span>Capital: </span>
+//             {capital}
+//         </>
+//     ) : ( " ")
+
+//     const formatLanguage = languages.length > 1 ? 'Languages' : 'Language'
+//     console.log(languages)
+//     return (
+//         <div className="country">
+//             <div className="country_flag">
+//                 <img src={flag} alt={name} />
+//             </div>
+//             <h3 className="country_name">{name.toUpperCase()}</h3>
+//             <div className="country_text">
+//                 <p>{formatedCapital}</p>
+//                 <p>
+//                     <span>{formatLanguage}: </span>
+//                     {languages.map((language) => language.name).join(', ')}
+//                 </p>
+//                 <p>
+//                     <span>Population: </span>
+//                     {population.toLocaleString()}
+//                 </p>
+//                 <p>
+//                     <span>Currency: </span>
+//                     {currency}
+//                 </p>
+//             </div>
+//         </div>
+//     )
+// }
+
+// class App extends Component {
+//     state= {
+//         data: [],
+//     }
+
+// componentDidMount() {
+//     const url = 'https://restcountries.eu/rest/v2/all'
+//     fetch(url)
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         this.setState({
+//             data,
+//         })
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+// }
+
+// render() {
+//     return ( 
+//         <div className="App">
+//             <h1>React Component Life Cycle</h1>
+//             <h1>Calling API</h1>
+//             <div>
+//                 <p>There are {this.state.data.length} countries in the api</p>
+//                 <div className="countries-wrapper">
+//                     {this.state.data.map((country) => (<Country country={country} />
+//                     ))}
+//                 </div>
+//             </div>
+//         </div>
+//         )
+//     }
+// }
+
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(<App />, rootElement)
+
+
+
+//!!!! FETCH
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const Country = ({
+  country: { name, capital, flag, languages, population, currency },
+}) => {
+  const formatedCapital =
+    capital.length > 0 ? (
+      <>
+        <span>Capital: </span>
+        {capital}
+      </>
+    ) : (
+      ''
+    )
+  const formatLanguage = languages.length > 1 ? `Languages` : `Language`
+  console.log(languages)
+  return (
+    <div className='country'>
+      <div className='country_flag'>
+        <img src={flag} alt={name} />
+      </div>
+      <h3 className='country_name'>{name.toUpperCase()}</h3>
+      <div class='country_text'>
+        <p>{formatedCapital}</p>
+        <p>
+          <span>{formatLanguage}: </span>
+          {languages.map((language) => language.name).join(', ')}
+        </p>
+        <p>
+          <span>Population: </span>
+          {population.toLocaleString()}
+        </p>
+        <p>
+          <span>Currency: </span>
+          {currency}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+class App extends Component {
+  state = {
+    data: [],
+  }
+
+  componentDidMount() {
+    this.fetchCountryData()
+  }
+
+    fetchCountryData = async () =>{
+        const url = 'https://restcountries.eu/rest/v2/all'
+        const response = await fetch(url)
+        const data = await response.json()
+        this.state({
+            data,
+        })
+    }
+
+    render() {
+        
+    }
+
+
+
+
+
+
+ //! AXIOS
+// import React, { Component } from 'react'
+// import ReactDOM from 'react-dom'
+// import axios from 'axios'
+
+// const Country = ({
+//   country: { name, capital, flag, languages, population, currency },
+// }) => {
+//   return (
+//     <div className='country'>
+//       <div className='country_flag'>
+//         <img src={flag} alt={name} />
+//       </div>
+//       <h3 className='country_name'>{name.toUpperCase()}</h3>
+//       <div class='country_text'>
+//         <p>
+//           <span>Population: </span>
+//           {population}
+//         </p>
+//         >
+//       </div>
+//     </div>
+//   )
+// }
+
+// class App extends Component {
+//   state = {
+//     data: [],
+//   }
+
+//   componentDidMount() {
+//     const url = 'https://restcountries.eu/rest/v2/all'
+//     axios
+//       .get(url)
+//       .then((response) => {
+//         this.setState({
+//           data: response.data,
+//         })
+//       })
+//       .catch((error) => {
+//         console.log(error)
+//       })
+//   }
+
+//   render() {
+//     return (
+//       <div className='App'>
+//         <h1>React Component Life Cycle</h1>
+//         <h1>Calling API</h1>
+//         <div>
+//           <p>There are {this.state.data.length} countries in the api</p>
+//           <div className='countries-wrapper'>
+//             {this.state.data.map((country) => (
+//               <Country country={country} />
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(<App />, rootElement)
